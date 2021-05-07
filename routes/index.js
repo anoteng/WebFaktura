cac0ed00a6656d7ce87fa675810f5d50cad84df1
Andreas Noteng <andreas@noteng.no>
2021-05-07 14:12:47 +0200

    Kundebehandling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+let menuItems = []
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.pug', { title: 'WebFaktura', pageId: req.params.pageId });
+  if(params.pageId === 'main' || params.pageId === false){
+    menuItems = []
+  }else if(params.pageId === 'customers') {
+    menuItems = [
+      {id: 'newCustomer',
+        text: 'Ny kunde'},
+      {id: 'showCustomers',
+        text: 'Vis kunder'}
+    ]
+  }
+  res.render('index.pug', { title: 'WebFaktura', pageId: req.params.pageId, menuItems: menuItems});
 });
 
 module.exports = router;
