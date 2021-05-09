65d86971390616d82a8ad65ae41a77c678bc55a3
Andreas Noteng <andreas@noteng.no>
2021-05-09 08:28:08 +0200

    lagre ny kunde

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../database');
+const sequelize = require('sequelize')
 
 router.get("/all", function(req, res) {
     db.Customer.findAll()
